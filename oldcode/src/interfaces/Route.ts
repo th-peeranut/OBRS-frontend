f06133a8f5e8de45f2f76dc5730f5b4be6356429
chutipong_jar <flukekazo55@gmail.com>
2024-10-16 20:34:06 +0700

    cmit/ remove oldcode

--- oldcode/src/interfaces/Route.ts
+++
@@ -1,8 +0,0 @@
-export default interface Route {
-    id: string,
-    name: string
-    stations: Array<string>
-    status: string
-    requestedBy: string
-    requestedDate: string
-}
--- oldcode/src/interfaces/Vehicle.ts
+++
@@ -1,9 +0,0 @@
-export default interface Vehicle {
-    id: null,
-    numberPlate: string
-    type: string
-    totalSeating: number
-    status: string
-    requestedBy: string
-    requestedDate: string
-}
