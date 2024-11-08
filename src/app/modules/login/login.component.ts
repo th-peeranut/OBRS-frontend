94e29c438527eeef4d8c46281613ee8dc8b658fd
chutipong_jar <flukekazo55@gmail.com>
2024-11-08 22:32:27 +0700

    mock login page with i18n

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  isDropdownOpen = false;
   currentLanguage: string = 'th';
 
-  constructor(private translate: TranslateService) {
+  @ViewChild('dropdownButton', { static: true }) dropdownButton!: ElementRef;
+
+   constructor(
+    private translate: TranslateService,
+    private renderer: Renderer2,
+    private elementRef: ElementRef
+  ) {
     this.translate.setDefaultLang('th');
     this.translate.use('th');
   }
 
   switchLanguage(lang: string) {
+    this.isDropdownOpen = false;
     this.currentLanguage = lang;
     this.translate.use(lang);
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+
+    if (this.isDropdownOpen) {
+      this.renderer.listen('document', 'click', (event: Event) => this.handleOutsideClick(event));
+    }
+  }
+
+  handleOutsideClick(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    const clickedInsideDropdown = this.elementRef.nativeElement.contains(targetElement);
+    const clickedDropdownButton = this.dropdownButton.nativeElement.contains(targetElement);
+
+    if (clickedInsideDropdown && clickedDropdownButton) {
+      this.isDropdownOpen = true;
+    }else{
+      this.isDropdownOpen = false;
+    }
   }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,7 +13,10 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     CommonModule,
     RouterModule,
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
     LoginComponent
