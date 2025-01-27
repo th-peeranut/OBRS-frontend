a6ad790c92232658d242a26899a5ba2fc7f9df6e
chutipong_jar <flukekazo55@gmail.com>
2025-01-27 22:55:27 +0700

    update home screen with draft icon and detail

--- src/app/modules/home/components/station-home/station-home.component.ts
+++ src/app/modules/home/components/station-home/station-home.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-station-home',
   templateUrl: './station-home.component.html',
-  styleUrl: './station-home.component.scss'
+  styleUrl: './station-home.component.scss',
 })
 export class StationHomeComponent {
+  isOn: boolean = false;
 
+  currentDirection: 'left' | 'right' = 'left'; // Default direction is 'left'
+
+  onObrsClick(direction: 'left' | 'right'): void {
+    if (this.currentDirection !== direction) {
+      this.currentDirection = direction;
+    }
+  }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,19 @@
+// Angular Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Third-Party Libraries
+import { TranslateModule } from '@ngx-translate/core';
+import { CalendarModule } from 'primeng/calendar';
+
+// Application Components
 import { HomeComponent } from './home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { RouterModule, Routes } from '@angular/router';
 import { HomeBookingComponent } from './components/home-booking/home-booking.component';
 import { DropdownObrsComponent } from '../../shared/components/dropdown-obrs/dropdown-obrs.component';
 import { DropdownObrsPassengerComponent } from './components/dropdown-obrs-passenger/dropdown-obrs-passenger.component';
-import { CalendarModule } from 'primeng/calendar';
-import { ReactiveFormsModule } from '@angular/forms';
 import { StationHomeComponent } from './components/station-home/station-home.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
@@ -17,6 +22,7 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
   declarations: [HomeComponent, NavbarComponent, HomeBookingComponent, StationHomeComponent],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
     TranslateModule.forChild({
