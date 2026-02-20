612a717068a50951e4615485010251897d2c8a9a
Chutipong Jarernsawat <flukekazo55@gmail.com>
2026-02-20 21:09:29 +0700

    fix: add env sit

--- src/environments/environment.sit.ts
+++ src/environments/environment.sit.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8000',
   apiUrl: 'https://sit-obrs-backend.koyeb.app:8000',
   promptpay: {
     baseUrl: 'https://promptpay.io',
